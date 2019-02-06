d03336d4e7f8f4caf541aa062adcf31fc7b186e5
AngelDyankov <angel.dyankoff@gmail.com>
2019-02-06 23:26:43 +0200

    Initial commit

---
+++ TcpClient/src/main/java/TcpClient.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+/**
+ * Created by blabla on 05-Feb-19.
+ */
+public class TcpClient {
+
+    public static void main(String args[]) throws Exception {
+        System.out.println("Starting client");
+        while(true) {
+            Socket clientSocket = null;
+            try {
+                clientSocket = new Socket(System.getenv("HOST"), Integer.parseInt(System.getenv("PORT")));
+                DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
+                BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+                outToServer.writeBytes("Hi boss " + System.lineSeparator());
+                outToServer.flush();
+                String modifiedSentence = inFromServer.readLine();
+                System.out.println("FROM SERVER: " + modifiedSentence);
+            } catch(Exception e) {
+                System.out.println("Still waiting: " + e.getMessage());
+            } finally {
+                if(clientSocket != null && !clientSocket.isClosed()) {
+                    clientSocket.close();
+                }
+            }
+            Thread.sleep(1000);
+        }
+    }
+}
